5030dec2a2408a8698bed6214d0325cc95a6e48c
annavlasand <anvlas1996@yandex.ru>
2023-01-24 15:36:45 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,11 @@
+import pandas as pd
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+from sklearn import tree
+
+titanic_data = pd.read_csv('titanic.csv', encoding='windows-1251', sep=',')
+titanic_data.isnull().sum() #определяем, сколько пропущено данных в переменных, возможно что-то стоит исключить
+
+X = titanic_data.drop(['PassengerId', 'Survived', 'Name', 'Ticket', 'Cabin'], axis=1)
+y = titanic_data.Survived
