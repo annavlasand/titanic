3d3f3886794de98a4d1b3124b185f035a3f4c7ef
annavlasand <anvlas1996@yandex.ru>
2023-01-24 16:37:32 +0300

    learning

--- main.py
+++ main.py
@@ -14,9 +14,23 @@ X = pd.get_dummies(X)
 X.Age.median()
 X = X.fillna({'Age': X.Age.median()})
 
-clf = tree.DecisionTreeClassifier()
+clf = tree.DecisionTreeClassifier(criterion='entropy', max_depth=5)
 clf.fit(X, y)
 
 from sklearn.model_selection import train_test_split
-X_train, X_test, y_test, y_test = train_test_split(X, y, test_size=0.33, random_state=42)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33, random_state=42)
 clf.score(X, y)
+
+#посмотрим, как влияет глубина выборки на точность предсказывания значений деревом решений
+scores_data = pd.DataFrame()
+max_depth_values = range(1, 100)
+for max_depth in max_depth_values:
+    clf = tree.DecisionTreeClassifier(criterion='entropy', max_depth=5)
+    clf.fit(X_train, y_train)
+    train_score = clf.score(X_train, y_train)
+    test_score = clf.score(X_test, y_test)
+
+    temp_score_data = pd.DataFrame({'max_depth': [max_depth],
+                                    'train_score': [train_score],
+                                    'test_score': [test_score]})
+    scores_data = scores_data.append(temp_score_data)
